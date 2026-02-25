7045501c9cfc56e65a54b4c5cb3971a674c075f6
BondarenkoDmitro <kinglion2579@gmail.com>
2026-02-25 23:07:36 +0200

    update 2.0

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-let KeyCode: number;
-let KeyCode2: string = "KeyCode2";
-let KeyCode3 = document.getElementById("Text-1") as HTMLInputElement;
-document.addEventListener("keypress", event => {
-    KeyCode = event.keyCode;
-    let KeyCode2: string = KeyCode.toString();
-    KeyCode3.textContent = KeyCode2;
-    console.log(event);
-});
+const codeElement = document.getElementById("Text-1");
+const keyElement = document.getElementById("formkey");
+const locationElement = document.getElementById("location");
 
+document.addEventListener("keydown", (event: KeyboardEvent) => {
+    if (codeElement) {
+        codeElement.textContent = event.keyCode;
+    }
+    if (keyElement) {
+        keyElement.textContent = event.key;
+    }
+    if (locationElement) {
+        locationElement.textContent = event.location.toString();
+    }
+});
